11e4acbe55eda16d9690eeac0e171a2d581042fa
GabrielBentoGroove <gabriel.bento@groove.tech>
2024-02-22 16:02:16 -0300

    Retired comments in coding

--- tests/support/actions/Leads.js
+++ tests/support/actions/Leads.js
@@ -10,22 +10,16 @@ export class Leads {
     }
 
     async openModal() {
-        // await page.click('//button[text()="Aperte o play... se tiver coragem"]');
         await this.page.getByRole('button', { name: /Aperte o play/}).click();
 
         await expect(this.page.getByTestId('modal')
           .getByRole('heading'))
           .toHaveText('Fila de espera');  
-        // await page.getByText('seus dados conosco').click();
-        // const content = await page.content();
-        // console.log(content);
     }
 
     async submitLeadForm(name, email) {
         await this.page.getByPlaceholder('Informe seu nome').fill(name);
         await this.page.locator('#email').fill(email);
-        // await page.locator('input[name=email]').fill('gabrielbento@gmail.com');
-        // await page.locator('input[placeholder="Seu email principal"]').fill('gabrielbento@gmail.com');
 
         await this.page.getByTestId('modal')    
             .getByText('Quero entrar na fila!').click();
@@ -36,7 +30,6 @@ export class Leads {
 
         await expect((toast)).toHaveText(message);
         await expect((toast)).not.toBeVisible({ timeout: 5000 });
-        // await expect((toast)).toBeHidden({ timeout: 5000 });
     }
 
     async alertHaveText(target) {
